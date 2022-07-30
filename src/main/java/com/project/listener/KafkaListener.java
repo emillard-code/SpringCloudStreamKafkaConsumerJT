3372cc7e65fb8476eb3bbf512ddc1080daef63f3
Emerson Millard <emillard.code@gmail.com>
2022-07-30 12:00:00 -0700

    Modified listener class.

--- src/main/java/com/project/listener/KafkaListener.java
+++ src/main/java/com/project/listener/KafkaListener.java
@@ -1,4 +1,22 @@
 package com.project.listener;
 
+import com.project.model.Book;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+
+@EnableBinding(Sink.class)
 public class KafkaListener {
+
+    private Logger logger = LoggerFactory.getLogger(KafkaListener.class);
+
+    @StreamListener("input")
+    public void consumeMessage(Book book) {
+
+        logger.info("Consume payload : " + book);
+
+    }
+
 }
