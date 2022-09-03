d7038d81c824ea0c1d790bb48df74b1d7ba25f15
Emerson Millard <emillard.code@gmail.com>
2022-09-03 00:37:43 -0700

    Initial commit

---
+++ src/test/java/com/project/springcloudstreamkafkaconsumerjt/SpringCloudStreamKafkaConsumerJtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.springcloudstreamkafkaconsumerjt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudStreamKafkaConsumerJtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
